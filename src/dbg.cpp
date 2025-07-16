f67a1a9591e86179d2cad5761ab9e7066d108d76
mxtlrr <ruson0914@gmail.com>
2025-07-16 13:42:00 -0500

    output debug strings

--- src/dbg.cpp
+++ src/dbg.cpp
@@ -1,5 +1,8 @@
 #include "dbg.hpp"
 
+#define YES_UNICODE 1
+#define NO_UNICODE  0
+
 std::vector<thread_t> threads;
 void HandleDbg(DEBUG_EVENT* d){
 	switch(d->dwDebugEventCode){
@@ -16,7 +19,7 @@ void HandleDbg(DEBUG_EVENT* d){
 			break;
 		}
 		
-			case EXIT_THREAD_DEBUG_EVENT:
+			case EXIT_THREAD_DEBUG_EVENT: {
 				// Check which thread is actually invalid
 				for(DWORD i = 0; i < threads.size(); i++){
 					if(threads[i].h == NULL){
@@ -25,12 +28,34 @@ void HandleDbg(DEBUG_EVENT* d){
 					}
 				}
 				break;
+			}
 
-			
+
+			case EXIT_PROCESS_DEBUG_EVENT:
+				MessageBoxA(NULL, "Plants vs. Zombies has quit. Pressing 'OK' will kill Flux.",
+							"WARNING", MB_ICONWARNING | MB_OK);
+				exit(0);
+				break;
+
+			case OUTPUT_DEBUG_STRING_EVENT: {
+				printf("flux debugger: recieved debug string event!\n");
+				
+				OUTPUT_DEBUG_STRING_INFO si = d->u.DebugString;
+				if(si.fUnicode != YES_UNICODE){
+					uint8_t* mm = (uint8_t*)malloc(si.nDebugStringLength+1);
+					
+					// Temporary handle for debugging
+					HANDLE hDbg = OpenProcess(PROCESS_ALL_ACCESS, FALSE, d->dwProcessId);
+					SIZE_T read = 0;
+					ReadProcessMemory(hDbg, si.lpDebugStringData, mm, (si.nDebugStringLength+1), &read);
+					printf("flux debugger: \"%s\"\n", mm);
+					free(mm);
+				}
+				break;
+			}
 
 			default:
-				printf("flux debugger: unhandled debug event %02ld\n",
-					d->dwDebugEventCode);
+				printf("flux debugger: unhandled debug event %ld\n", d->dwDebugEventCode);
 				break;
 	}
 }
